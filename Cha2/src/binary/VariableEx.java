a981abe83c3189ea53b2e6beb90b86209bcb0b3d
youngseoJi <estell.ys.j@gmail.com>
2022-10-04 15:56:51 +0900

    변수 간단하게 복습

---
+++ Cha2/src/binary/VariableEx.java
@@ -0,0 +1,18 @@
+package binary;
+
+public class VariableEx {
+
+	public static void main(String[] args) {
+
+		int num; 
+		num = 10;
+		
+		System.out.println(num);
+		
+		int level = 20;
+		System.out.println(level);
+		
+	
+	}
+
+}
