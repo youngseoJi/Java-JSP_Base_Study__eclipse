53a319c0817910e57b17c0381eb2302121ac52a7
youngseoJi <estell.ys.j@gmail.com>
2022-10-05 11:29:05 +0900

    연산자 복습

---
+++ Cha3/src/assignment/OperationEx.java
@@ -0,0 +1,27 @@
+package assignment;
+
+public class OperationEx {
+
+	public static void main(String[] args) {
+		
+		/* 대입 연산자 (단항)
+		* 우선순위가 가장 낮는 연산자 , 모든 연산 후 마지막에 대입됨
+		* 변수의 값을 변경하는 연산자
+		*/
+		int age = 24;
+		System.out.println(age); // 24
+		
+		age = 40;
+		System.out.println(age); // 40
+	
+		/* 부호 연산자 (단항)
+		* 양수 / 음수의 표현, 값의 부호를 변경하는 연산자
+		*/
+		int num = 10;
+		int num2 = -num;
+		
+		System.out.println(num); // 10
+		System.out.println(num2); // -10
+	}
+
+}
---
+++ Cha3/src/assignment/OperationEx2.java
@@ -0,0 +1,53 @@
+package assignment;
+
+public class OperationEx2 {
+
+	public static void main(String[] args) {
+
+		/* 산술연산자 (단항)
+		* 사칙연산에 사용됨 : / * - + %
+		* % 는 나머지 구하는 연산자 : 숫자 n의 나머지는 0 ~ n-1 범위의수*/
+		
+		/* 증가 감소 연산자 (단항) ++ -- 
+		* 1만큼 더하거나 뺄때 사용함 
+		* 항의 앞/뒤 위치에 따라 연산 결과가 달라짐*/
+		
+		// 항의 앞에 ++(--) 있는 경우
+		int num1 = 10;
+		//-> 출력이 되기전에 +1 일을 더해라
+		System.out.println(++num1); // 11
+		
+		// 항의 뒤에 ++(--)있는 경우 
+		int num2 = 10;
+		//-> 출력이 된 후에  
+		System.out.println(num2++); // 10
+		// +1 일을 더해라
+		System.out.println(num2); // 11
+		
+		/* 관계연산자 (이항)
+		 * 연산의 결과가 true / false로 변환됨 */
+		
+		System.out.println(4 > 3); // true 
+		
+		
+		int num3 = 3;
+		int num4 = 4; 
+		boolean flag = (num4 < num3); // 4 < 3 ?
+		System.out.println(flag); // false
+		
+		/* 논라연산자  && , ||
+		 * 연산의 결과가 true / false로 변환됨 
+		 * && : 둘다 true 여야함
+		 * || : 둘 중 하나만  true여도 됨*/
+		int num10 = 10;
+		int i = 2;
+		// 20 < 10 ? false 뒤에 식은 실행되지 않고, false 값이 value에 대입된다.
+		boolean value = (((num10 = num10 + 10) < 10) && ((i-11+2) > 10 ) );
+		System.out.println(value); // false
+		System.out.println(num10); // 20
+		System.out.println(i); // 2
+
+		
+	}
+
+}
