61dab105e82a1023c168a7d234941c02d7d093cb
youngseoJi <estell.ys.j@gmail.com>
2022-10-05 17:35:44 +0900

    test

--- cha5/src/cha5/Student.java
+++ cha5/src/cha5/Student.java
@@ -1,10 +1,18 @@
 package cha5;
 
 // 클래스 생성하기 : 학생
+// class(예약어) class명 {} 
 public class Student {
 	
+	// 변수 생성
 	int studentID; // 학생 식별자
 	String studentName; // 학생이름
 	int grade; // 학년
 	String address; // 주소
+	
+	// 메서드 셍성
+	
+	public void showStudentInfor() {
+		System.out.println(studentName + " , " + address);
+	}
 }
