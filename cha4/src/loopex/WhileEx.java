9e05fd2940aa00400024f2217d227fff2cdba370
youngseoJi <estell.ys.j@gmail.com>
2022-10-05 15:02:38 +0900

    반복문 do while 문 비교 while

--- cha4/src/loopex/WhileEx.java
+++ cha4/src/loopex/WhileEx.java
@@ -4,16 +4,48 @@ public class WhileEx {
 
 	public static void main(String[] args) {
 
+		// 반복문 while 문
 		int num = 1;
 		int sum = 0;
 		
-		// 반목문 조건 - 참인경우 무한반복
+		// while(반복문 조건) :  참인경우 무한반복
 		while(num <= 10) {
 			sum += num;
 			num++;
 		}
+		
 		// false 거짓인 겨우 while문 탈출, {} 밖의 코드 출력
 		System.out.println("1~10까지 합은 " + sum + "입니다 .");	
-	}
+	
+		
 
+		
+		
+		// 반복문 do .. while문
+		int num2 = 1;
+		int sum2 = 0;
+	
+		// do : 꼭 한번 실행 한다.
+		do {
+			sum2 += num2;
+			num2++;
+		}
+		// while(반복문 조건) :   참인경우 무한반복
+		while(num2 < 1 );
+		// false 거짓인 겨우 while문 탈출, {} 밖의 코드 출력
+		System.out.println(sum2 + "입니다 .");	 // 1입니다 .
+
+	
+
+		int num3 = 1;
+		int sum3 = 0;
+		while(num3 < 1) {
+			sum3 += num3;
+			num3++;
+		}
+		// false 거짓인 겨우 while문 탈출, {} 밖의 코드 출력
+		System.out.println( sum3 + "입니다 ."); // 0입니다 .
+
+
+	}
 }
