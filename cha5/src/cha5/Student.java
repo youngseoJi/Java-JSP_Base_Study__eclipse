ddce0bdb5331cd47ce7a9674fc21b0492aaf9469
youngseoJi <estell.ys.j@gmail.com>
2022-10-07 13:38:15 +0900

    클래스/객체 - 생성자

--- cha5/src/cha5/Student.java
+++ cha5/src/cha5/Student.java
@@ -11,6 +11,17 @@ public class Student {
 	int grade; 			// 학년
 	String address; 	// 주소
 	
+	// 자바 컴파일러는 생성자가 하나도 없을경우, 디폴트 생성자를 자동 제공함 (매개변수가 X)
+	// -> public Student(){}
+	// 직접 디폴트 생성자를 생성해도된다.
+	public Student() {}
+	
+	// 매개변수가 필요한 경우는 생성자를 직접 만들어준다.
+	public Student(int id, String name, String address) {
+		studentID = id;
+		studentName = name;
+	}
+	
 	// 메서드 셍성 - 반환값이 없는 경우 void를 적음
 	public void showStudentInfor() {
 		System.out.println(studentName  + " , " + address); 
--- cha5/src/cha5/StudentTest.java
+++ cha5/src/cha5/StudentTest.java
@@ -4,16 +4,17 @@ public class StudentTest {
    // 클래스를 사용, 실행하는 메서드  메인 메서드 생성 - class/객체를 사용하기 위한 함수
 	public static void main(String[] args) {
 	
-		// new 키워드 : (객체 /생성자/인스턴스) 생성하는 키워드 
+		// new 키워드 : (객체 /생성자/인스턴스) 호출하는 키워드 
 		// 참조형 데이터 타입 Student - 참조변수 studentJi
-		Student studentJi = new Student();
+		Student studentJi = new Student(1, "지씨", "서울시 도렴동");
 		
 		// 참조변수  .을 찍으면 참조 함수 사용가능 
 		// 메서드 실행 (클래스 실행, 사용하기)
-		studentJi.studentName = "지씨";
+		/*studentJi.studentName = "지씨";
 		studentJi.studentID = 1;
-		studentJi.address = "서울시 도렴동";
+		studentJi.address = "서울시 도렴동";*/
 		
+		// 디폴트 생성자 new로 호출하여 사용
 		Student studentKim = new Student();
 		
 		studentKim.studentName = "김씨";
