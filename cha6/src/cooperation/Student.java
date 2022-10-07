498af90b24f13c8c6a29569780e63e1612e05773
youngseoJi <estell.ys.j@gmail.com>
2022-10-07 17:35:30 +0900

    라인 수정

--- cha6/src/cooperation/Student.java
+++ cha6/src/cooperation/Student.java
@@ -23,13 +23,12 @@ public class Student {
 	}
 	
 	// 학생이 지하철 탈 때 호출 메소드 (반환값 없는 함수) - 매개변수 (생성된 버스 인스턴스)
-		public void takeSubway(Subway subway) {
-			// subway클래스의 .take 함수 : 비용 1500원 + 되고, 승객수 +1 되는 함수 
-			subway.take(1500);
+	public void takeSubway(Subway subway) {
+	// subway클래스의 .take 함수 : 비용 1500원 + 되고, 승객수 +1 되는 함수 
+		subway.take(1500);
 			// 탑승비 1500원을 - 지출한다. 
-			money -= 1500;
-			
-		}
+		money -= 1500;
+	}
 	
 	public void showInfo() {
 		System.out.println(studentName + "님의 남은 돈은" + money + "원 입니다");
