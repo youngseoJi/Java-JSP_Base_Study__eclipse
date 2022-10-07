f9ca9c4fc82bccd4dbe5cafe8f067c8e089e5de5
youngseoJi <estell.ys.j@gmail.com>
2022-10-07 11:26:00 +0900

    클래스 객체: 인스턴스 생성 및 클래스 사용하기

--- cha5/src/cha5/Student.java
+++ cha5/src/cha5/Student.java
@@ -6,10 +6,10 @@ package cha5;
 public class Student {
 	
 	// 변수 생성
-	int studentID; // 학생 식별자
+	int studentID; 		// 학생 식별자
 	String studentName; // 학생이름
-	int grade; // 학년
-	String address; // 주소
+	int grade; 			// 학년
+	String address; 	// 주소
 	
 	// 메서드 셍성 - 반환값이 없는 경우 void를 적음
 	public void showStudentInfor() {
@@ -24,22 +24,5 @@ public class Student {
 	public void setStudentName(String name) {
 		studentName = name;
 	}
-	
-	
-	
-	// 클래스를 사용하기 위한  메인 메서드가 해당 클래스 내에 있을 수 있지만,다른 클래스에서 작동할 수 있다.
-	
-/*	// 메인 메서드 (함수) 실행 : 해당 클래스를 실행하기 위한 메서드
-	public static void main(String[] args) {
-		
-		Student studentJI= new Student();
-		studentJI.studentName = "영떠";
-		studentJI.address = "서울시 서초구";
-		
-		// 메서드 실행 (클래스 실행, 사용하기)
-		studentJI.showStudentInfor();
-	}
-	*/
-	
 	
 }
--- cha5/src/cha5/StudentTest.java
+++ cha5/src/cha5/StudentTest.java
@@ -1,15 +1,34 @@
 package cha5;
 
 public class StudentTest {
-   // 클래스를 사용, 실행하는 메서드
+   // 클래스를 사용, 실행하는 메서드  메인 메서드 생성 - class/객체를 사용하기 위한 함수
 	public static void main(String[] args) {
-	Student studentJI= new Student();
-			studentJI.studentName = "영떠";
-			studentJI.address = "서울시 서초구";
-			
-			// 메서드 실행 (클래스 실행, 사용하기)
-			studentJI.showStudentInfor(); // 영떠 , 서울시 서초구
-
+	
+		// new 키워드 : (객체 /생성자/인스턴스) 생성하는 키워드 
+		// 참조형 데이터 타입 Student - 참조변수 studentJi
+		Student studentJi = new Student();
+		
+		// 참조변수  .을 찍으면 참조 함수 사용가능 
+		// 메서드 실행 (클래스 실행, 사용하기)
+		studentJi.studentName = "지씨";
+		studentJi.studentID = 1;
+		studentJi.address = "서울시 도렴동";
+		
+		Student studentKim = new Student();
+		
+		studentKim.studentName = "김씨";
+		studentKim.studentID = 2;
+		studentKim.address = "서울시 봉천동";
+		
+		studentJi.showStudentInfor();
+		studentKim.showStudentInfor();
+		
+		System.out.println(studentJi);
+		System.out.println(studentKim);
 	}
-
+			
+   // 클래스를 사용하기 위한  메인 메서드가 해당 클래스 내에 있을 수 있지만,다른 클래스에서 작동할 수 있다.
+			
 }
+
+
