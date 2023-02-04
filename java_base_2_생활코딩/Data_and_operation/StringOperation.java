b065bb59d33b0d9baed649544678942a0df90f48
지영서 <estell.ys.j@gmail.com>
2023-02-05 02:50:51 +0900

    Add / 문자열 타입 & 문자열 연산 , 메소드

---
+++ java_base_2_생활코딩/Data_and_operation/StringOperation.java
@@ -0,0 +1,17 @@
+
+// 문자열 연산자
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+
+		// length() : 문자열의 길이 
+		System.out.println("hello world".length());
+		
+		// replace(바뀔문자, 바꿀문자):
+		System.out.println("hello, name!".replace("name", "mh")); // hello, mh!
+		
+		// https://docs.oracle.com/javase/8/docs/api/java/lang/String.html
+	}
+
+}
