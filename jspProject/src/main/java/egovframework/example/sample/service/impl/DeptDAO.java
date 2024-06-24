e72a92374d0b5fe942f2339c40700af0e7612d00
지영서 <estell.ys.j@gmail.com>
2024-06-24 11:46:34 +0900

    Add : 서비스 설정 - DB연결 DAO 기본 생성완

--- jspProject/src/main/java/egovframework/example/sample/service/impl/DeptDAO.java
+++ jspProject/src/main/java/egovframework/example/sample/service/impl/DeptDAO.java
@@ -1,7 +1,19 @@
 package egovframework.example.sample.service.impl;
 
+import org.egovframe.rte.psl.dataaccess.EgovAbstractDAO;
+import org.springframework.stereotype.Repository;
+
+import egovframework.example.sample.service.DeptVO;
+
 //sql 가기전의중간다리
 // DB연결담당 -> sql로 연결된다.
-public class DeptDAO {
+
+@Repository("deptDAO")
+public class DeptDAO  extends EgovAbstractDAO {
+
+	public String InsertDept(DeptVO vo) {
+		// TODO Auto-generated method stub
+		return (String) insert("deptDAO.insertDept", vo);
+	}
 
 }
