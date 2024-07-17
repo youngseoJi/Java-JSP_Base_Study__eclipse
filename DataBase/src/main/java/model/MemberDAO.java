b46dba6a8498be8240141f446b4d3ac9688811f9
지영서 <estell.ys.j@gmail.com>
2024-07-17 16:59:53 +0900

    Update : JSP 데이터 베이스 연동 - MemberList 구현/ 모든 회원의 정보를 리턴해주는 메소드 생성하기

--- DataBase/src/main/java/model/MemberDAO.java
+++ DataBase/src/main/java/model/MemberDAO.java
@@ -79,9 +79,54 @@ public class MemberDAO {
 	//return type : Vector<MemberBean> 
 	
 	public Vector<MemberBean> allSelectMember() {
-		
+		// 가변길이로 데이터를 저장한다.
 		Vector<MemberBean> v = new Vector<>();
 		
+		// 무조건 데이터베이스, 스레드, 입출력(IO), 네트워크와 같은 패키지에서 메소드를 사용할 때는 반드시 에러 처리를 해야 합니다. 
+		try {
+			// 커넥션 연결 
+			getCon();
+			
+			// 쿼리 준비 
+			String sql = "select * from member;";
+			// 쿼리를 실행시주는 객체 선언
+			pstmt = con.prepareStatement(sql);
+			// 쿼리를 실행시킨 결과를 리턴해서 받아줌 (오라클 테이블의 검색된 결과를 자바객체(ResultSet)에 저장)
+			// -> ResultSet에 결과를 저장
+			rs = pstmt.executeQuery();
+			
+			// 반복문을 이용하여 rs에 저장된 데이터를 추출하기
+			
+			// 조건 : 저장된 데이터 만큼 반복문을 돌리겠다.     
+			while(rs.next()) {
+				
+				// 모든 회원 정보를 메모리에 저장하는 역할 - 컬럼으로 나뉜 데이터를 빈 클래스에 저장한다.
+				MemberBean bean = new MemberBean();
+				
+				bean.setId(rs.getString(1));
+				bean.setPassword(rs.getString(2));
+				bean.setEmail(rs.getString(3));
+				bean.setTel(rs.getString(4));
+				bean.setHobby(rs.getString(5));
+				bean.setAge(rs.getString(6));
+				bean.setJob(rs.getString(7));
+				bean.setInfo(rs.getString(8));
+				
+				// 패키징된 memberBean 클래스를 백터에 저장
+				// 0번 인덱스 부터 순서대로 데이터 저장 
+				v.add(bean);
+				
+			}
+			
+			// 자원 반납 - 데이터베이스 연 종료
+			con.close();
+			
+		} catch (Exception e) {
+			
+			
+		}
+		// 모든 데이터가 저장된 백터를 반환함
+		// 반환되 객체는 데이터베이스에서 조회된 모든 회원 정보를 포함하며, 이후의 웹 페이지에 정보 출력에 사용	
 		return v;
 	}
 	
