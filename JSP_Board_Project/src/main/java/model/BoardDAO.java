f6ecf98a4c6378202b2d1b092a50c3326b29b7d7
지영서 <estell.ys.j@gmail.com>
2024-07-31 09:45:09 +0900

    Add: BoardInfo 게시글 1개 조회 기능 및 화면 구현
    num을 이용하여 게시글을 조회
    답글 수정 삭제 기능 과 연결예정

--- JSP_Board_Project/src/main/java/model/BoardDAO.java
+++ JSP_Board_Project/src/main/java/model/BoardDAO.java
@@ -11,128 +11,178 @@ import javax.sql.DataSource;
 
 public class BoardDAO {
 
-    // 데이터베이스에 접근할 수 있도록 설정
-    Connection connection = null;
-    // 데이터 베이스에서 쿼리를 실행시켜주는 객체
-    PreparedStatement preparedStatement = null;
-    // 데이터베이스의 테이블의 결과를 리턴받아 자바에 저장해주는 객체
-    ResultSet resultSet = null;
+	// 데이터베이스에 접근할 수 있도록 설정
+	Connection conn = null;
+	// 데이터 베이스에서 쿼리를 실행시켜주는 객체
+	PreparedStatement pstmt = null;
+	// 데이터베이스의 테이블의 결과를 리턴받아 자바에 저장해주는 객체
+	ResultSet rs = null;
 
-    // 데이터 베이스의 커넷션풀을 사용하도록 설정하는 메소드
-    public void getConnection() {
+	// 데이터 베이스의 커넥션풀을 사용하도록 설정하는 메소드
+	public void getConnection() {
 
-        try {
-            // JNDI 초기 컨텍스트를 얻는다.
-            Context initContext = new InitialContext();
+		try {
+			// JNDI 초기 컨텍스트를 얻는다.
+			Context initCtx = new InitialContext();
 
-            // 톰캣 서버에 정보를 담아놓은 곳으로 이동
-            Context envContext = (Context) initContext.lookup("java:comp/env");
+			// 톰캣 서버에 정보를 담아놓은 곳으로 이동
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
 
-            // 데이터 소스 객체 선언
-            DataSource dataSource = (DataSource) envContext.lookup("jdbc/pool");
+			// 데이터 소스 객체 선언
+			DataSource ds = (DataSource) envCtx.lookup("jdbc/pool");
 
-            // 데이터 소스를 기준으로 커넥션을 연결하기
-            connection = dataSource.getConnection();
+			// 데이터 소스를 기준으로 커넥션을 연결하기
+			conn = ds.getConnection();
 
-        } catch (Exception e) {
-            System.out.println("DB 연결 실패: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
+		} catch (Exception e) {
+			System.out.println("DB 연결 실패: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
-    // 새로운 게시글 생성하기 (저장하기)
-    // 입력폼에 입력한 게시글 데이터 저장하기
-    public void insertBoard(BoardBean boardBean) {
+	// 새로운 게시글 생성하기 (저장하기)
+	// 입력폼에 입력한 게시글 데이터 저장하기
+	public void insertBoard(BoardBean boardBean) {
 
-        getConnection();
+		getConnection();
 
-        // 빈클래스에 넘어오지 않았던 데이터 초기화 작업
-        int ref = 0; // 글그룹 : 쿼리를 실행시켜서 가장큰 ref 값을 갖고와 ref+1을 더해줘야함.
-        int reStep = 1; // 새글 : 부모글
-        int reLevel = 1;
+		// 빈클래스에 넘어오지 않았던 데이터 초기화 작업
+		int ref = 0; // 글그룹 : 쿼리를 실행시켜서 가장큰 ref 값을 갖고와 ref+1을 더해줘야함.
+		int reStep = 1; // 새글 : 부모글
+		int reLevel = 1;
 
-        try {
-            // 가장큰 ref 값 갖고오는 쿼리 
-            String refSql = "select max(ref) from board";
+		try {
+			// 가장큰 ref 값 갖고오는 쿼리
+			String refSql = "select max(ref) from board";
 
-            // 쿼리 실행 후 결과 리턴하기
-            preparedStatement = connection.prepareStatement(refSql);
-            resultSet = preparedStatement.executeQuery();
+			// 쿼리 실행 후 결과 리턴하기
+			pstmt = conn.prepareStatement(refSql);
+			rs = pstmt.executeQuery();
 
-            if (resultSet.next()) { // 결과값이 있다
-                ref = resultSet.getInt(1) + 1; // 최대값에 +1을 더해서 글그룹을 설정한다.
-            }
+			if (rs.next()) { // 결과값이 있다
+				ref = rs.getInt(1) + 1; // 최대값에 +1을 더해서 글그룹을 설정한다.
+			}
 
-            // 실제로 게시글 전체값을 테이블에 저장 
-            /** 자동 설정되는 필드값 설정
-             * num : board_seq.NEXTVAL 각 게시글에 고유한 번호를 부여하는데 사용 / 시퀀스는 데이터베이스에서 자동으로 증가하는 숫자를 생성하는데 사용
-             * read_data: systata 게시글이 데이터베이스에 삽입되는 순간의 시간을 자동으로 기록 현재 시스템에 저장된 시간의 값 / 데이터베이스 서버의 현재 날짜와 시간을 반환
-             * read_count : 새 게시글의 조회수는 0으로 초기화 / 게시글이 처음 작성되었을 때 아직 조회되지 않았다는 의미
-             * */
-            String sql = "insert into board (num, writer, email, title, password, reg_date, ref, re_step, re_level, read_count, content) "
-                    + "values (board_seq.NEXTVAL, ?, ?, ?, ?, sysdate, ?, ?, ?, 0, ?)";
+			// 실제로 게시글 전체값을 테이블에 저장
+			/**
+			 * 자동 설정되는 필드값 설정 num : board_seq.NEXTVAL 각 게시글에 고유한 번호를 부여하는데 사용 / 시퀀스는
+			 * 데이터베이스에서 자동으로 증가하는 숫자를 생성하는데 사용 read_data: sysdate 게시글이 데이터베이스에 삽입되는 순간의 시간을
+			 * 자동으로 기록 현재 시스템에 저장된 시간의 값 / 데이터베이스 서버의 현재 날짜와 시간을 반환 read_count : 새 게시글의 조회수는
+			 * 0으로 초기화 / 게시글이 처음 작성되었을 때 아직 조회되지 않았다는 의미
+			 */
+			String sql = "insert into board (num, writer, email, title, password, reg_date, ref, re_step, re_level, read_count, content) "
+					+ "values (board_seq.NEXTVAL, ?, ?, ?, ?, sysdate, ?, ?, ?, 0, ?)";
 
-            preparedStatement = connection.prepareStatement(sql);
+			pstmt = conn.prepareStatement(sql);
 
-            preparedStatement.setString(1, boardBean.getWriter());
-            preparedStatement.setString(2, boardBean.getEmail());
-            preparedStatement.setString(3, boardBean.getTitle());
-            preparedStatement.setString(4, boardBean.getPassword());
-            preparedStatement.setInt(5, ref);
-            preparedStatement.setInt(6, reStep);
-            preparedStatement.setInt(7, reLevel);
-            preparedStatement.setString(8, boardBean.getContent());
+			pstmt.setString(1, boardBean.getWriter());
+			pstmt.setString(2, boardBean.getEmail());
+			pstmt.setString(3, boardBean.getTitle());
+			pstmt.setString(4, boardBean.getPassword());
+			pstmt.setInt(5, ref);
+			pstmt.setInt(6, reStep);
+			pstmt.setInt(7, reLevel);
+			pstmt.setString(8, boardBean.getContent());
 
-            //쿼리실행
-            preparedStatement.executeUpdate();
-            // 자원반납
-            connection.close();
+			// 쿼리실행
+			pstmt.executeUpdate();
+			// 자원반납
+			conn.close();
 
-        } catch (Exception e) {
-            System.out.println("데이터베이스 삽입 에러: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
+		} catch (Exception e) {
+			System.out.println("데이터베이스 삽입 에러: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
-    // 모든 게시글 조회하기 
-    public Vector<BoardBean> getAllBoard() {
-        // BoardBean 타입의 객체들을 저장할 Vector 타입 인스턴스를 생성합니다.
-        Vector<BoardBean> boardList = new Vector<>();
-        getConnection();
+	// 모든 게시글 조회하기
+	public Vector<BoardBean> getAllBoard() {
+		// BoardBean 타입의 객체들을 저장할 Vector 타입 인스턴스를 생성합니다.
+		Vector<BoardBean> boardList = new Vector<>();
+		getConnection();
 
-        try {
-            // 쿼리 준비 : 
-            String sql = "select * from board order by ref desc, re_step asc";
+		try {
+			// 쿼리 준비 :
+			String sql = "select * from board order by ref desc, re_step asc";
 
-            // 쿼리 실행할 객체 선언
-            preparedStatement = connection.prepareStatement(sql);
+			// 쿼리 실행할 객체 선언
+			pstmt = conn.prepareStatement(sql);
 
-            // 쿼리 실행 후 결과 저장
-            resultSet = preparedStatement.executeQuery();
+			// 쿼리 실행 후 결과 저장
+			rs = pstmt.executeQuery();
 
-            // 반복분을 이용하여 데이터를 추출 : 데이터가 몇개인지 모르기떄문 
-            while (resultSet.next()) {
-                // 데이터 패키징 (BoardBean 클래스이용)
-                BoardBean boardBean = new BoardBean();
-                boardBean.setNum(resultSet.getInt(1));
-                boardBean.setWriter(resultSet.getString(2));
-                boardBean.setEmail(resultSet.getString(3));
-                boardBean.setTitle(resultSet.getString(4));
-                boardBean.setPassword(resultSet.getString(5));
-                boardBean.setReg_date(resultSet.getDate(6).toString());
-                boardBean.setRef(resultSet.getInt(7));
-                boardBean.setRe_step(resultSet.getInt(8));
-                boardBean.setRe_level(resultSet.getInt(9));
-                boardBean.setRead_count(resultSet.getInt(10));
-                boardBean.setContent(resultSet.getString(11));
+			// 반복문을 이용하여 데이터를 추출 : 데이터가 몇개인지 모르기 때문에
+			while (rs.next()) {
+				// 데이터 패키징 (BoardBean 클래스 이용)
+				BoardBean boardBean = new BoardBean();
+				boardBean.setNum(rs.getInt(1));
+				boardBean.setWriter(rs.getString(2));
+				boardBean.setEmail(rs.getString(3));
+				boardBean.setTitle(rs.getString(4));
+				boardBean.setPassword(rs.getString(5));
+				boardBean.setReg_date(rs.getDate(6).toString());
+				boardBean.setRef(rs.getInt(7));
+				boardBean.setRe_step(rs.getInt(8));
+				boardBean.setRe_level(rs.getInt(9));
+				boardBean.setRead_count(rs.getInt(10));
+				boardBean.setContent(rs.getString(11));
 
-                // 패키징한 데이터 백터 저
-                boardList.add(boardBean);
-            }
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return boardList;
-    }
+				// 패키징한 데이터 백터에 저장
+				boardList.add(boardBean);
+			}
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return boardList;
+	}
+
+	// 특정 게시물 조회하기
+	public BoardBean getOneBoard(int num) {
+		// 리턴 타입 선언 / 결과를 저장할 BoardBean 객체 생성
+		BoardBean board = new BoardBean();
+		getConnection();
+
+		try {
+			// 조회수 증가 쿼리 : 게시글을 클릭하면 조회수가 +1 씩증가한다. 
+			String readCountSql = "update board set read_count = read_count+1 where num=?";
+			
+			pstmt = conn.prepareStatement(readCountSql);
+			pstmt.setInt(1, num);
+			
+			pstmt.executeUpdate();
+			
+			
+			// 특정 게시글 조회 쿼리 : 번호 num인 특정 게시글 1개를 조회
+			String readSql = "select * from board where num=?";
+
+			// 쿼리 실행 객체
+			pstmt = conn.prepareStatement(readSql);
+			pstmt.setInt(1, num);
+
+			// 쿼리 실행 결과 리턴
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				board.setNum(rs.getInt(1));
+				board.setWriter(rs.getString(2));
+				board.setEmail(rs.getString(3));
+				board.setTitle(rs.getString(4));
+				board.setPassword(rs.getString(5));
+				board.setReg_date(rs.getDate(6).toString());
+				board.setRef(rs.getInt(7));
+				board.setRe_step(rs.getInt(8));
+				board.setRe_level(rs.getInt(9));
+				board.setRead_count(rs.getInt(10));
+				board.setContent(rs.getString(11));
+			}
+			conn.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// 조회한 게시물 정보가 담긴 BoardBean 객체 반환
+		return board;
+	}
 }
---
+++ JSP_Board_Project/src/main/webapp/BoardInfo.jsp
@@ -0,0 +1,140 @@
+<%@page import="model.BoardBean"%>
+<%@page import="model.BoardDAO"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>게시글 조회</title>
+
+<style>
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
+
+body {
+	width: 100%;
+	text-align: center;
+	display: flex;
+	justify-content: center;
+	margin: 20px;
+}
+
+h2 {
+	margin: 10px;
+}
+
+table {
+	width: 100%;
+	border-collapse: collapse;
+	border: 1px solid gray;
+}
+
+tr {
+	height: 40px;
+	border: 1px solid gray;
+}
+
+td {
+	width: 100px;
+	border: 1px solid gray;
+	padding: 5px 20px;
+}
+
+.board-content {
+	text-align: left;
+	width: 200px;
+}
+
+.content {
+	height: 300px;
+	vertical-align: top;
+	padding: 10px 20px; /* 패딩값 유지 */
+}
+
+.board-title {
+	width: 100px;
+}
+
+button {
+	padding: 4px 8px;
+}
+</style>
+
+</head>
+<body>
+
+	<%
+	// request로부터 전달된 파라미터 num(string타입)을 정수형으로 변환하여 가져온다. 
+	int num = Integer.parseInt(request.getParameter("num").trim());
+	/* String num = request.getParameter("num"); */
+
+	// 데이터베이스 접근을 위한 BoardDAO 객체 생성
+	BoardDAO boardDAO = new BoardDAO();
+
+	/* 게시글 갖고오기 
+	num에 해당하는 특정 게시글의 정보를 데이터베이스에서 조회하고,
+	이를 게시글 BoardBean 객체 타입으로 가져온다.*/
+	BoardBean board = boardDAO.getOneBoard(num);
+	%>
+
+	<section>
+		<!-- 게시글 보여질 정보
+			 1. 번호
+			 2. 작성자
+			 3. 제목 (1줄)
+			 4. 작성날짜
+			 5. 조회수
+			 6. 글내용(1줄)
+			 7. 이메
+			 
+	 -->
+		<h2>게시글 보기</h2>
+		<table>
+			<tr>
+				<td class="board-title">번호</td>
+				<td class="board-content"><%=board.getNum()%></td>
+				<td class="board-title">조회수</td>
+				<td class="board-content"><%=board.getRead_count()%></td>
+			</tr>
+			<tr>
+				<td class="board-title">작성자</td>
+				<td class="board-content"><%=board.getWriter()%></td>
+				<td class="board-title">작성일</td>
+				<td class="board-content"><%=board.getReg_date()%></td>
+			</tr>
+			<tr>
+				<td class="board-title">제목</td>
+				<td colspan="4"><%=board.getTitle()%></td>
+			</tr>
+			<tr>
+				<td class="board-title">내용</td>
+				<td class="board-content content" colspan="4"><%=board.getContent()%></td>
+			</tr>
+			<tr>
+				<td class="board-title">이메일</td>
+				<td class="board-content" colspan="4"><%=board.getEmail()%></td>
+			</tr>
+			<!-- 버튼 
+					1. 답글쓰기
+					2. 수정하기
+					3. 삭제하기
+					4. 전체목록
+			-->
+			 <tr>
+				<td class="control-btns" colspan="4">
+					<button
+						onclick="location.href='BoardReWriteForm.jsp?num=<%=board.getNum()%>&ref=<%=board.getRef()%>%&re_step=<%=board.getRe_step()%>%&re_level=<%=board.getRe_level()%>'">답글</button>
+					<button onclick="location.href='BoardUpdateForm.jsp?num=<%=board.getNum()%>'">수정</button>
+					<button onclick="location.href='BoardDeleteForm.jsp?num=<%=board.getNum()%>'">삭제</button>
+					<button onclick="location.href='BoardList.jsp'">목록</button>
+				</td>
+			</tr> 
+		</table>
+	</section>
+
+</body>
+</html>
